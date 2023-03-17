26b14b5817671b93fd0d9a506b2bbff5cd2e4c42
zuyoe <9xtwo5@naver.com>
2023-03-17 19:15:41 +0900

    헤더 스크롤 추가

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  // 스크롤 기능
+  // 스크롤바의 상단위치
+  let scy = 0;
+  let scActive = 50;
+  scy = window.document.documentElement.scrollTop;
+
+  let header = document.querySelector(".header");
+
+  header.addEventListener("mouseenter", function () {
+    header.classList.add("header-active");
+  });
+  header.addEventListener("mouseleave", function () {
+    if (scy < scActive) {
+      header.classList.remove("header-active");
+    }
+  });
+
+  // 새로고침 시
+  if (scy > scActive) {
+    header.classList.add("header-active");
+  }
+
+  window.addEventListener("scroll", function () {
+    scy = window.document.documentElement.scrollTop;
+    // console.log("스크롤 : " + scy);
+    if (scy > scActive) {
+      header.classList.add("header-active");
+    } else {
+      header.classList.remove("header-active");
+    }
+  });
+};
