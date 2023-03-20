eadb68943dad4cf3caca0f5b9d3226ef68dac4d3
zuyoe <9xtwo5@naver.com>
2023-03-20 16:24:17 +0900

    슬라이더 수정중

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
 window.onload = function () {
-  // 스크롤 기능
-  // 스크롤바의 상단위치
-  let scy = 0;
-  let scActive = 50;
-  scy = window.document.documentElement.scrollTop;
-
-  let header = document.querySelector(".header");
-
-  header.addEventListener("mouseenter", function () {
-    header.classList.add("header-active");
-  });
-  header.addEventListener("mouseleave", function () {
-    if (scy < scActive) {
-      header.classList.remove("header-active");
-    }
-  });
-
-  // 새로고침 시
-  if (scy > scActive) {
-    header.classList.add("header-active");
-  }
-
-  window.addEventListener("scroll", function () {
+    // 스크롤 기능
+    // 스크롤바의 상단위치
+    let scy = 0;
+    let scActive = 50;
     scy = window.document.documentElement.scrollTop;
-    // console.log("스크롤 : " + scy);
+
+    let header = document.querySelector(".header");
+
+    header.addEventListener("mouseenter", function () {
+        header.classList.add("header-active");
+    });
+    header.addEventListener("mouseleave", function () {
+        if (scy < scActive) {
+            header.classList.remove("header-active");
+        }
+    });
+
+    // 새로고침 시
     if (scy > scActive) {
-      header.classList.add("header-active");
-    } else {
-      header.classList.remove("header-active");
+        header.classList.add("header-active");
     }
-  });
+
+    window.addEventListener("scroll", function () {
+        scy = window.document.documentElement.scrollTop;
+        // console.log("스크롤 : " + scy);
+        if (scy > scActive) {
+            header.classList.add("header-active");
+        } else {
+            header.classList.remove("header-active");
+        }
+    });
+
+    // 비주얼 슬라이드 기능
+    const swiper = new Swiper(".swVisual", {
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 };
